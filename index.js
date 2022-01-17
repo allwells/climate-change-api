e03afd28e15ad95dc920e796d3f700d9f4a1be1a
allwells <aleenfestus@gmail.com>
2022-01-17 16:16:19 +0100

    Install pkgs
    Signed-off-by: allwells <aleenfestus@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const PORT = 3000
+const http = require("http")
+const axios = require("axios")
+const cheerio = require("cheerio")
+const express = require("express")
+const app = express()
+const server = http.createServer(app)
+
+
+server.listen(port=PORT, hostname="localhost", backlog=() => {
+    console.log(`Server running on port ${PORT}`)
+})
