77f98306af872884a5f62b7e871cc3a0366a9297
allwells <aleenfestus@gmail.com>
2022-01-18 03:27:21 +0100

    Update project files
    Signed-off-by: allwells <aleenfestus@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const PORT = 3001
 const http = require("http")
 const app = require("./app")
 const server = http.createServer(app)
 
+const PORT = process.env.PORT || 8000
 server.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 })
